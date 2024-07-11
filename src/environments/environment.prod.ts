59c26dd0488fd0d9fff2f30cbb7945f56526c07d
unknown <iliya.melishev@gmail.com>
2024-07-11 14:29:34 +0300

    Configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://jsonplaceholder.typicode.com'
+  baseUrl: 'https://jsonplaceholder.typicode.com',
+  currencyURL: 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=demo'
 };
